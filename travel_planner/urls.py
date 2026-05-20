3d2bf7293d35e95b21d67947bdaa1a791903bf17
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-05-20 14:35:56 +0300

    Add projects/<int:project_pk>/places/ and projects/<int:project_pk>/places/<int:pk>/ endpoints to travel_planner

---
+++ travel_planner/urls.py
@@ -0,0 +1,16 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from travel_planner.views import ProjectPlaceViewSet, TravelProjectViewSet
+
+router = DefaultRouter()
+router.register('projects', TravelProjectViewSet, basename='project')
+
+project_place_list = ProjectPlaceViewSet.as_view({'get': 'list', 'post': 'create'})
+project_place_detail = ProjectPlaceViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('projects/<int:project_pk>/places/', project_place_list, name='project-place-list'),
+    path('projects/<int:project_pk>/places/<int:pk>/', project_place_detail, name='project-place-detail'),
+]
